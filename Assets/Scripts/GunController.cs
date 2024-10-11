f8085d2c135b16b3d46546e03c41e18437238211
KonstantinasArelis <arelis.konstantinas@gmail.com>
2024-10-11 18:29:58 +0300

    added player, enemy, uzi  models, made a few tweaks

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -7,6 +7,8 @@ public class GunController : MonoBehaviour
     [SerializeField] private float bulletSpeed = 20.0f;
     [SerializeField] private float shotCooldownSeconds = 0.5f;
     [SerializeField] private float bulletDisposeSeconds = 2.0f;
+    [SerializeField] private float bulletHeightOffset = 0.5f; // New variable for height offset
+
     private float lastShotTime = 0.0f;
 
     public void Fire()
@@ -15,8 +17,10 @@ public class GunController : MonoBehaviour
         bool gunCooledDown = lastShotDifference >= shotCooldownSeconds;
         if (gunCooledDown)
         {
-            GameObject bulletInstance = Instantiate(bulletPrefab, transform.position + transform.up, transform.rotation);
-            bulletInstance.GetComponent<Rigidbody>().AddForce(transform.up * bulletSpeed, ForceMode.Impulse);
+            // Apply the height offset to the bullet's starting position
+            Vector3 bulletSpawnPosition = transform.position + transform.up * bulletHeightOffset; 
+            GameObject bulletInstance = Instantiate(bulletPrefab, bulletSpawnPosition, Quaternion.identity);
+            bulletInstance.GetComponent<Rigidbody>().AddForce(transform.right * bulletSpeed * -1, ForceMode.Impulse);
             lastShotTime = Time.time;
             StartCoroutine(DisposeBulletCoroutine(bulletInstance));
         }
