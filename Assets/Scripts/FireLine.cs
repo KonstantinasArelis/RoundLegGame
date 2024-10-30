a7896b16cd9e603e045e21f75471ce5b65d60fe8
KonstantinasArelis <arelis.konstantinas@gmail.com>
2024-10-30 17:16:09 +0200

    refactored the gun logic again, fixed muzzle flashes for guns, enabled shadows for gun flashes

--- Assets/Scripts/FireLine.cs
+++ Assets/Scripts/FireLine.cs
@@ -3,39 +3,20 @@ using UnityEngine.VFX;
 
 public class FireLine : MonoBehaviour
 {
-    	private Vector3 initalForward;
-    	LineRenderer lineRenderer; 
-    	private readonly float lineDistance = 10f;
-    	[SerializeField] public float lineDuration = 0.1f;
-		[SerializeField] public float muzzleFlashDuration = 0.1f;
-    	[SerializeField] private float shotCooldownSeconds = 0.03f;
-    	private float lastShotTime = 0.0f;
-    	public VisualEffect muzzleFlash;
-		public Light muzzleFlashLight;
-
+    private Vector3 initalForward;
+    LineRenderer lineRenderer; 
+    private readonly float lineDistance = 10f;
+    [SerializeField] public float lineDuration = 0.1f;
+	
     void Start()
     {
-		muzzleFlashLight.enabled = false;
     	lineRenderer = GetComponent<LineRenderer>(); 
     	initalForward = transform.forward;
     }
     
     public void Fire()
     {
-		
-			float lastShotDifference = Time.time - lastShotTime;
-				bool gunCooledDown = lastShotDifference >= shotCooldownSeconds;
-			if (!gunCooledDown)
-			{
-				return;
-			}
-			
-			muzzleFlashLight.enabled = true;
-			Invoke(nameof(DisableMuzzleFlashLight), muzzleFlashDuration); 
-
-			muzzleFlash.Play();
-					lastShotTime = Time.time;
-					Vector3 direction = transform.forward;
+			Vector3 direction = transform.forward;
 			Vector3 endPoint = transform.position + direction * lineDistance;  
 			lineRenderer.SetPosition(0, transform.position);
 			lineRenderer.SetPosition(1, endPoint); 
@@ -56,11 +37,6 @@ public class FireLine : MonoBehaviour
 						zombieController.TakeDamage(1);
 					}
 			}
-    }
-    
-	void DisableMuzzleFlashLight()
-    {
-        muzzleFlashLight.enabled = false;
     }
 	
     void DisableLineRenderer()
