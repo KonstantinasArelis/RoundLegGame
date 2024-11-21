130c294e6f3aee53c15a9558c3c20746526177c1
Dominykas <113127442+Dom1nyk4s@users.noreply.github.com>
2024-11-21 10:17:54 +0200

    Pause Menu

--- Assets/Scripts/MonoBehaviour/Controllers/Humanoids/ZombieController.cs
+++ Assets/Scripts/MonoBehaviour/Controllers/Humanoids/ZombieController.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Linq;
-using UnityEditor.SceneManagement;
+// using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class ZombieController : EnemyController
