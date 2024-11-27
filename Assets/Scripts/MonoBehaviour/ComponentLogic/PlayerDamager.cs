00f6105a3df29c9951bd4fa8e23f6292268c3353
IgnasJo <jogminasignas@gmail.com>
2024-11-27 23:44:27 +0200

    shadowball changes

--- Assets/Scripts/MonoBehaviour/ComponentLogic/PlayerDamager.cs
+++ Assets/Scripts/MonoBehaviour/ComponentLogic/PlayerDamager.cs
@@ -5,12 +5,12 @@ public class PlayerDamager : MonoBehaviour
   public int damage = 1;
 
   void OnTriggerEnter(Collider other)
-  {
+  { 
     if (other.gameObject.CompareTag("Player"))
     {
       other.gameObject.GetComponent<PlayerController>().TakeDamage(damage);
-      Destroy(gameObject);
     }
+    Destroy(gameObject);
   } 
 
 }
